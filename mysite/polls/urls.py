6ce36c13bf47d676dd664fe8f18d890a56cf3c9b
Alex Shevelo <alex.shevelo@gmail.com>
2022-04-19 20:31:58 +0300

    init

---
+++ mysite/polls/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('asdasadaasdx/', views.index2, name='index'),
+    path('question/', views.QuestionList.as_view(), name='QuestionList'),
+    path('question/<int:question_id>/', views.QuestionDetails.as_view(), name='QuestionDetails')
+]

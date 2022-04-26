9a0345e33b7dfa87d74a94455e0f3af641c0a271
NoNameAnonym <89348432+NoNameAnonym@users.noreply.github.com>
2022-04-26 08:48:29 +0000

    Add files via upload

---
+++ serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import Question, Choice
+
+    
+class QuestionListSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Question
+        fields = ['id', 'pub_date', 'question_text']
+
+
+class QuestionDetailsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Question
+        fields = ['id', 'pub_date','question_text']
+
+
+
+
+
+class ChoiceListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = ['id', 'question','choice_text','votes']
+
+
+class ChoiceDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = ['id', 'question', 'choice_description','votes','choice_text']
+
+
+
+
---
+++ views.py
@@ -0,0 +1,38 @@
+from django.http import HttpResponse
+from .models import Question, Choice
+from .serializers import QuestionListSerializer, QuestionDetailsSerializer,ChoiceDetailsSerializer,ChoiceListSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+    
+
+def index2(request):
+    return HttpResponse("index2index2index2index2index2Hello, world. You're at the polls index.")
+    
+
+class QuestionList(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionListSerializer
+    
+
+class QuestionDetails(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = QuestionDetailsSerializer
+    
+    def get_object(self):
+        return get_object_or_404(Question, pk=self.kwargs.get('question_id'))
+
+
+class ChoiceList(generics.ListCreateAPIView):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceListSerializer
+
+
+class ChoiceDetails(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ChoiceDetailsSerializer
+
+    def get_object(self):
+        return get_object_or_404(Choice, pk=self.kwargs.get('choice_id'))
+

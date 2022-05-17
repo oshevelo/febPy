03f52bb04953a689c984006c7bd25c31f69ebd02
annc4st <annaseniuta@gmail.com>
2022-05-17 15:34:35 +0100

    orders moels corrected

--- eshop/apps/orders/models.py
+++ eshop/apps/orders/models.py
@@ -6,13 +6,21 @@ from django.db import models
 import datetime
 from django.utils import timezone
 from django.core.exceptions import ValidationError
+import django.contrib.auth #?? installed apps
 
 
-class Orders(models.Model):
-    order_id = models.IntegerField(max_length=12)
-    #OR AND ?
-    primary_key = True
+class ShippingAddress(models.Model):
+    name = models.CharField('Full name', max_length=512)  # imja polychatelia
+    address_line1 = models.CharField('Address line 1', max_length=512)
+    address_line2 = models.CharField('Address line 2', max_length=512)
+    post_code = models.CharField('Postcode', max_length=8)
+    city = models.CharField('City', max_length=56)
+    country = models.CharField('Country', max_length=56)
 
+    class Meta:
+        verbose_name = "Shipping Address"
+
+class Order(models.Model):
 
     NEW = 'New'
     PAID = 'Paid'
@@ -28,21 +36,31 @@ class Orders(models.Model):
         ('COMPLETED', 'Completed'),
     ]
 
-    date_created = models.DateTimeFields('date created')
-    order_type = models.Charfield(max_lengths=20, choices=ORDER_TYPE, default=NEW)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
-    item = models.ManyToManyField(Item, on_delete=models.CASCADE)
+    created = models.DateTimeField('Created')
+    order_type = models.CharField(max_length=20, choices=ORDER_TYPE, default=NEW)
 
-    def validate_order_value(order_value):
-        if order_value <= 0:
-            raise ValidationError('Total price for the order should be more than 0 UAH')
+    #orders can be placed without the user authenticating  -> do not need to have customer ID
+    user = models.ForeignKey('auth.User', on_delete=models.PROTECT)
+    shipping_address = models.ForeignKey(ShippingAddress, on_delete=models.CASCADE) #null-True, blank=True???
 
 
     def __str__(self):
         return self.order_type, self.primary_key
 
 
+class OrderItem(models.Model):
+    #product = models.ForeignKey('products.Product', on_delete=models.SET_NULL, null=True) #eshe nyjno napisat class Product
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
 
+    @property
+    def order_value(self):
+        return sum([Product.item.price * item.quantity for item in self.order_items.all()])  # ??
+    # question product item - kak pisat
 
+        ## gde ety fynkciu pisat : v klase Orders ili OrderItem
+    def validate_order_value(order_value):
+        if order_value <= 0:
+            raise ValidationError('Total price for the order should be more than 0')
 
 

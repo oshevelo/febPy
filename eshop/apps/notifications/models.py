91158f5897cb3288b73056063004401168a7b821
lunoly <zfreid@i.ua>
2022-05-25 16:15:50 +0300

    notifications 1.0

--- eshop/apps/notifications/models.py
+++ eshop/apps/notifications/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Notification(models.Model):
+    SENDMETHODS = (
+        ('email', 'email'),
+        ('telegram', 'telegram'),
+        ('viber', 'viber'),
+        ('sms', 'sms'),
+    )
+
+    recipient = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    message_text = models.CharField(max_length=200)
+    time_stamp = models.DateTimeField(auto_now_add=True)
+    send_method = models.CharField(choices=SENDMETHODS, max_length=20)
+    subject = models.CharField(max_length=200)
+
 
 # Create your models here.
--- eshop/eshop/settings.py
+++ eshop/eshop/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'apps.userprofiles',
     'apps.polls',
     'apps.feedbacks',
+    'apps.notifications',
 ]
 
 MIDDLEWARE = [

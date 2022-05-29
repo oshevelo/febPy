e944f4dc05c585a9288c27f7dfb9d25bcd0125e9
lunoly <zfreid@i.ua>
2022-05-29 21:20:10 +0300

    notification_branch

---
+++ eshop/apps/notifications/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User	
+from rest_framework import serializers
+from apps.notifications.models import Notification
+
+
+
+
+class NotificationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Notification
+        fields = ['recipient', 'message_text', 'send_method','subject']
+        
+class NotificationDetailSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Notification
+        fields = ['recipient', 'message_text', 'send_method','subject']
+        
+        
+

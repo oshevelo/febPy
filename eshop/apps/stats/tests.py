6a0f64b971e63e02412b9dc6ee8348ccb1ce0895
Anastasia <malamut1986@gmail.com>
2022-06-05 21:18:47 +0200

    add tests

--- eshop/apps/stats/tests.py
+++
@@ -1,3 +0,0 @@
-from django.test import TestCase
-from rest_framework.test import APIClient
-
--- eshop/apps/stats/tests/tests_stat.py
+++ eshop/apps/stats/tests/tests_stat.py
@@ -1,37 +1,32 @@
 from django.test import TestCase
-from rest_framework.test import APIClient
 from rest_framework import status
+from rest_framework.test import APIClient
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
-from apps.polls.models import Question
-from collections import OrderedDict
-from datetime import datetime
+from apps.stats.models import UserAction
+
 from django.utils import timezone
 
 
-class RecalculationProfileChildrenChanges(TestCase):
-
-    def setUp(self):
+class UserActionTest(TestCase):
+    def setUp(self) -> None:
         self.c = APIClient()
         user_kw = dict(
-            username='asmin',
+            username='admin',
             password='111',
-            email='asmin' + '.com'
+            email='admin' + '@gmail.com',
         )
         user_kw['password'] = make_password(user_kw['password'])
         self.user = User.objects.create(**user_kw)
 
     def test_list_check_permission(self):
-        response = self.c.get('/api/polls/question/')
-
+        response = self.c.get('/stat/')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_list_empty(self):
         self.c.login(username=self.user.username, password='111')
-        response = self.c.get('/api/polls/question/')
-
+        response = self.c.get('/stat/')
         print(response.data)
-        # print(dir(status))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, {
             "count": 0,
@@ -40,18 +35,24 @@ class RecalculationProfileChildrenChanges(TestCase):
             "results": []
         })
 
-    def test_list_empty(self):
-        new_question = Question.objects.create(question_text='asdasd', pub_date=datetime.now())
-        new_question = Question.objects.create(question_text='asdasd', pub_date=datetime.now(), author=self.user)
+    def test_list_no_empty(self):
+        new_user_action = UserAction.objects.create(user=User.objects.get(username='admin'), data='', pub_date=timezone.now())
         self.c.login(username=self.user.username, password='111')
-        response = self.c.get('/api/polls/question/')
-
+        response = self.c.get('/stat/')
         print(response.data)
-        # print(dir(status))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, {
-            "count": 0,
+            "count": 1,
             "next": None,
             "previous": None,
-            "results": []
+            "results": [
+                {
+                    "id": 1,
+                    "user": "admin",
+                    'data': "",
+                    "pub_date": timezone.now()
+                }
+            ]
         })
+
+

8d50ad838333759c95ee5c27454b71aa99ce48ad
Anastasia <malamut1986@gmail.com>
2022-06-05 13:55:14 +0200

    add filters, permission

---
+++ eshop/apps/stats/filters.py
@@ -0,0 +1,13 @@
+import django_filters
+
+from django_filters.rest_framework import FilterSet
+
+from .models import UserAction
+
+
+class UserActionFilter(FilterSet):
+    user = django_filters.CharFilter(field_name="user", lookup_expr='icontains')
+
+    class Meta:
+        model = UserAction
+        fields = ['pub_date', 'user']
--- eshop/apps/stats/views.py
+++ eshop/apps/stats/views.py
@@ -1,11 +1,17 @@
 from .models import UserAction
 from .serializers import UserActionSerializer
-from rest_framework import generics
+from rest_framework import generics, pagination
+from rest_framework.permissions import IsAuthenticated
+from .filters import UserActionFilter
 
 
 class UserActionList(generics.ListCreateAPIView):
     queryset = UserAction.objects.all()
     serializer_class = UserActionSerializer
+    pagination_class = pagination.LimitOffsetPagination
+    filterset_class = UserActionFilter
+    permission_classes = [IsAuthenticated]
+
 
 
 

3e273f6430aae79b7f4b7832fe0c663ad9a4d8d5
rg <roman.grabovskiy@hotmail.com>
2022-06-06 16:26:20 +0300

    feedback/fix user create

--- eshop/utils/helpers_for_tests.py
+++ eshop/utils/helpers_for_tests.py
@@ -6,10 +6,17 @@ from django.contrib.auth.models import User
 def create_user(username, superuser=False, user_kwargs_dict=None):
     user_kwargs_dict = user_kwargs_dict or dict()
 
+    set_staff = False
+
+    if superuser:
+        set_staff = True
+
     user_kw = dict(
         username=username,
         password='111',
         email=username + '@email.com',
+        is_active=True,
+        is_staff=set_staff,
         is_superuser=superuser
     )
 

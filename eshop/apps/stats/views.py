034b18ade850fa284f0fce03fa106325f263bfe8
Anastasia <malamut1986@gmail.com>
2022-06-26 15:00:18 +0200

    add function to models

--- eshop/apps/stats/views.py
+++ eshop/apps/stats/views.py
@@ -6,15 +6,13 @@ from .filters import UserActionFilter
 
 
 class UserActionList(generics.ListCreateAPIView):
-    queryset = UserAction.objects.order_by('data__action')[::10] #return only current user actions from 0 to 10
+    queryset = UserAction.objects.order_by('data__action')[:10] #return only current user actions from 0 to 10
     serializer_class = UserActionSerializer
     pagination_class = pagination.LimitOffsetPagination
     filterset_class = UserActionFilter
     permission_classes = [IsAuthenticated]
 
-    def count_time(self, user): #add time to using site
-        return UserAction.object.filter(data__action='logout', user=user).pub_date()-\
-            UserAction.object.filter(data__action='login', user=user).pub_date()
+
 
 
 
